a5a7f80c264ebb92a54bf9c17335200ccfa7f46b
Johnlawniczak <lawniczak.john@gmail.com>
2023-08-16 22:00:38 -0400

    add comments

--- tests/unit/test_dup_hawk.py
+++ tests/unit/test_dup_hawk.py
@@ -1,8 +1,33 @@
+from unittest.mock import patch
 import pandas as pd
-
+import pytest
 from dup_hawk import mark_duplicates_from_dfs
 
+#  TO RUN: pytest test_dup_hawk.py
 
-def test_mark_duplicates_from_dfs(df, dist_df):
-    response = mark_duplicates_from_dfs(df, dist_df)
+# Sample data as pytest fixtures
+
+
+@pytest.fixture
+def sample_df():
+    # Replace with data
+    data = {
+        'number': [1, 2, 3],
+        'title': ["Issue 1", "Issue 2", "Issue 3"],
+        'body': ["Description 1", "Description 2", "Description 3"]
+    }
+    return pd.DataFrame(data)
+
+
+@pytest.fixture
+def sample_dist_df():
+    # Replace with data
+    data = {
+        # Your distance data here
+    }
+    return pd.DataFrame(data)
+
+
+def test_mark_duplicates_from_dfs(sample_df, sample_dist_df):
+    response = mark_duplicates_from_dfs(sample_df, sample_dist_df)
     assert response == {3: [2], 2: [3]}
